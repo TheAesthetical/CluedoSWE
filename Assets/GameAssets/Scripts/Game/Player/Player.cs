f853963032295e6c47183fc3ca2bf2d4e0cb24e3
oliverisyes <152007306+oliverisyes@users.noreply.github.com>
2026-04-20 13:41:32 +0100

    Start of player class

---
+++ Assets/GameAssets/Scripts/Game/Player/Player.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Player
+{
+    private int ID;
+    // private CharacterCard character; // maybe
+    private int position;
+    // private Card[] hand;
+    private bool eliminated; // maybe
+
+    public void makeSuggestion()
+    {
+
+    }
+
+    public void makeAccusation()
+    {
+
+    }
+
+    public void showCard()
+    {
+
+    }
+
+    public void canDisprove(string suggestion) // string is placeholder until suggestion class
+    {
+
+    }
+
+    public void eliminate()
+    {
+        eliminated = true;
+    }
+}
