e974f132431ad3d72944d61cde1f96de313b7528
Bailey <115033956+BaileyB5647@users.noreply.github.com>
2026-04-20 13:01:34 +0100

    Completed Cards, Deck, and MurderEnvelope code.

---
+++ Assets/GameAssets/Scripts/Cards/CharacterCard.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+/// <summary>
+///  Class <c>CharacterCard</c> represents a Character card in the game. 
+/// </summary>
+public class CharacterCard : Card
+{
+    /// <summary>
+    /// Instantiates a new instance of the <c>CharacterCard</c> class.
+    /// </summary>
+    /// <param name="CardNameIn"> The name of the character. </param>
+    public CharacterCard(string CardNameIn) : base(CardNameIn)
+    {
+    }
+
+}
---
+++ Assets/GameAssets/Scripts/Cards/WeaponCard.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+/// <summary>
+/// Class <c>WeaponCard</c> represents a Weapon Card in the game.
+/// </summary>
+public class WeaponCard : Card
+{
+    /// <summary>
+    /// Instantiates a new instance of the <c>WeaponCard</c> class. 
+    /// </summary>
+    /// <param name="CardNameIn"> The name of the weapon. </param>
+    public WeaponCard(string CardNameIn) : base(CardNameIn)
+    {
+    }
+
+}
